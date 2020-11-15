72c1c578e921368ff7d9dd4b6790b14c181ea3b9
RomanStepanyan <stepik10@gmail.com>
2020-11-15 21:16:08 +0200

    Images are added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./sass/main.scss";
+
+import "./partials/js/initialHomePage.js"
+import "./partials/js/navigation.js"
+import "./partials/js/filmDetailsPage.js"
+import "./partials/js/libraryPage.js"
